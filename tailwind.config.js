70475bfea3b2fde894ae2b29371134c66d70e336
Bogdan Bulakh <95144713+bbulakh@users.noreply.github.com>
2023-02-07 18:31:38 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./components/**/*.{html,js}', './**/*.html'],
+  theme: {
+    extend: {
+      gridTemplateRows: {
+        '[auto,auto,1fr]': 'auto auto 1fr',
+      },
+    },
+  },
+  plugins: [ require('@tailwindcss/aspect-ratio')],
+}
